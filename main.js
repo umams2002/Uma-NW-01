36cd63bc0dc856364784043a9208ce07e26639b7
Uma M Subramanian <umamaheswari.subramanian@hrblock.com>
2023-07-04 16:50:20 -0500

    updated code

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 document.getElementById("nameInput").addEventListener("input", () => { 
    let user = document.getElementById("nameInput").value;
-   let welcome_string = `Greetings ${user}!.Time is ${Date()}`;
+   let welcome_string = `Greetings ${user}!`;
    document.getElementById("welcomeOutput").innerText = welcome_string;
 });
 
